6cfd4e4fddd02c19b42029d51afc8b26c4bc32ec
maneeshcoder <maneeshrawat143@gmail.com>
2026-09-10 15:52:35 +0530

    fix samesite

--- server/src/utils/cookie.util.ts
+++ server/src/utils/cookie.util.ts
@@ -1,17 +1,17 @@
 import { response, Response } from "express";
 
 const REFRESH_COOKIE_NAME = "refreshToken";
-
-export function setRefreshCookie(res:Response,token : string){
-    res.cookie(REFRESH_COOKIE_NAME,token,{
-        httpOnly:true,
-        secure:process.env.NODE_ENV=="production",
-        sameSite:"none",
-        maxAge:7*24*60*60*1000,
-        path:"/api/auth",
+const isProd = process.env.NODE_ENV === 'production';
+export function setRefreshCookie(res: Response, token: string) {
+    res.cookie(REFRESH_COOKIE_NAME, token, {
+        httpOnly: true,
+        secure: isProd,
+        sameSite: isProd ? 'none' : 'lax',
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+        path: "/api/auth",
     });
 }
 
-export function clearRefreshCookie(res:Response){
-    res.clearCookie(REFRESH_COOKIE_NAME,{path:"/api/auth"});
+export function clearRefreshCookie(res: Response) {
+    res.clearCookie(REFRESH_COOKIE_NAME, { path: "/api/auth" });
 }
