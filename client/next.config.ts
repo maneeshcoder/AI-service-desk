ae94d45fc7ba4fbfb450568fbfcf0dfe783de945
maneeshcoder <maneeshrawat143@gmail.com>
2026-09-10 15:02:38 +0530

    fix next.config.ts

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,8 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  output: "standalone",
+  output: process.env.VERCEL ? undefined : 'standalone',
 };
 
 export default nextConfig;
